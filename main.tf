5ea8ff396581f6e2f43ba194704bb88e130c750e
triiyanshu <triiyanshujena@outlook.com>
2023-02-27 22:03:06 +0530

    adding basic request specs

--- main.tf
+++ main.tf
@@ -1,1 +1,32 @@
-test
+resource "aws_sqs_queue" "queue" {
+    name  = "cd-dev-issue-package-builder-queue"
+    tags = {
+        Environment = "dev"
+    }
+}
+
+resource "aws_sqs_queue" "queue" {
+    name  = "cd-uat-issue-package-builder-queue"
+    tags = {
+        Environment = "uat"
+    }
+}
+
+resource "aws_sqs_queue" "queue" {
+    name  = "cd-live-issue-package-builder-queue"  
+
+    tags = {
+        Environment = "live"
+    } 
+}
+
+resource "aws_sqs_queue" "terraform_queue_deadletter" {
+    name = "cd-dev-issue-package-builder-dlq"
+}
+
+resource "aws_sqs_queue" "terraform_queue_deadletter" {
+    name = "cd-uat-issue-package-builder-dlq"
+}
+resource "aws_sqs_queue" "terraform_queue_deadletter" {
+    name = "cd-live-issue-package-builder-dlq"
+}
